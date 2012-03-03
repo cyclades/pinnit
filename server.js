05570ed9d14b086e4502a03f591ae8476adf6c51
Alex Hu <alex@identified.com>
2012-03-03 02:46:01 -0800

    Added express server

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
-var static  = require('node-static');
-var file    = new(static.Server)('./public');
+var http = require('http');
+var express = require('express');
+var jade = require('jade');
+var app = express.createServer();
 
-require('http').createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response);
+app.set('view options',{layout: false});
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/jsrv/', function(req, res){
+  var reddit = {host: 'www.reddit.com',
+                port: 80,
+                path: '/.json'};
+
+  http.get(reddit, function(response) {
+   var bodyArr = [];
+   response.on('data', function (chunk) {
+     bodyArr.push(chunk.toString());
+   });
+   response.on('end', function (chunk) {
+     res.send(JSON.parse(bodyArr.join('')));
+   });
   });
-}).listen(8888);
+
+});
+
+app.get('/', function(req, res) {
+  res.render('index.jade');
+});
+
+
+app.listen(3000);
